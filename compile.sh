2bf6f417aa86e86fa9b192e9c96b8c945668bb12
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2021-01-13 12:59:58 +0300

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf out
-python bob.py $1 \
-&& time (yes | ffmpeg -framerate $1 -start_number 0 -i out/frame_%d.png -c:v libx264 -crf 5 -preset ultrafast out.mp4)
+python bob.py $1 $2 $3 \
+&& time (yes | ffmpeg -framerate $3 -start_number 0 -i out/frame_%d.png -c:v libx264 -crf 5 -preset ultrafast out.mp4)
