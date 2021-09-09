5beee076dcdd6c8cc57e82747cd51ec65e7453bf
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2021-09-09 14:51:32 +0300

    Move threaded_cache into the main folder

--- lanim/pil_utils.py
+++ lanim/pil_utils.py
@@ -3,7 +3,7 @@ import hashlib
 from pathlib import Path
 import shutil
 from PIL import Image
-from threaded_cache import threaded_cache
+from lanim.threaded_cache import threaded_cache
 from latex import render_latex_to_png
 
 
